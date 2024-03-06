4828bd4963d44fbcc513526c635f2d714bc72c3a
kashishdalal1 <kashish.metaloop@gmail.com>
2024-03-06 20:45:29 +0530

    8:45 6/3/2024 Kashish Dalal

--- lib/signature.dart
+++ lib/signature.dart
@@ -5,7 +5,8 @@ import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:signature/signature.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
-// Import the new share_plus dependency
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'package:share_plus/share_plus.dart';
 
 void main() {
@@ -47,85 +48,95 @@ class _SignaturePadExampleState extends State<SignaturePadExample> {
       appBar: AppBar(
         title: Text('Signature Pad Example'),
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
             child: Container(
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
+                border: Border.all(color: Colors.black), // Set border color
               ),
-              child: Signature(
-                controller: _controller,
-                height: double.infinity,
-                backgroundColor: Colors.white, // Set background color
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Signature(
+                  controller: _controller,
+                  height: double.infinity,
+                  backgroundColor: Colors.white, // Set background color
+                ),
               ),
             ),
           ),
           Container(
             color: Colors.grey[200],
-            child: Column(
-              children: [
-                Slider(
-                  value: _penStrokeWidth,
-                  min: 1,
-                  max: 20,
-                  onChanged: (value) {
-                    setState(() {
-                      _penStrokeWidth = value;
-                      _updatePenSettings();
-                    });
-                  },
-                  label: _penStrokeWidth.round().toString(),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    DropdownButton<Color>(
-                      value: _selectedColor,
-                      onChanged: (color) {
-                        setState(() {
-                          _selectedColor = color!;
-                          _updatePenSettings();
-                        });
-                      },
-                      items: [
-                        DropdownMenuItem(
-                          value: Colors.black,
-                          child: Text('Black'),
-                        ),
-                        DropdownMenuItem(
-                          value: Colors.blue,
-                          child: Text('Blue'),
-                        ),
-                        DropdownMenuItem(
-                          value: Colors.red,
-                          child: Text('Red'),
-                        ),
-                      ],
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _controller.clear(); // Clear the content
-                        });
-                      },
-                      child: Text('Clear'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final signatureBytes = await _controller.toPngBytes();
-                        final signaturePath =
-                        await saveSignatureAsImage(signatureBytes!);
-                        // Call the new method for sharing
-                        await shareSignatureImage(signaturePath);
-                      },
-                      child: Text('Save & Share'),
-                    ),
-                  ],
-                ),
-              ],
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Slider(
+                    value: _penStrokeWidth,
+                    min: 1,
+                    max: 20,
+                    onChanged: (value) {
+                      setState(() {
+                        _penStrokeWidth = value;
+                        _updatePenSettings();
+                      });
+                    },
+                    label: _penStrokeWidth.round().toString(),
+                  ),
+                  DropdownButton<Color>(
+                    value: _selectedColor,
+                    onChanged: (color) {
+                      setState(() {
+                        _selectedColor = color!;
+                        _updatePenSettings();
+                      });
+                    },
+                    items: [
+                      DropdownMenuItem(
+                        value: Colors.black,
+                        child: Text('Black'),
+                      ),
+                      DropdownMenuItem(
+                        value: Colors.blue,
+                        child: Text('Blue'),
+                      ),
+                      DropdownMenuItem(
+                        value: Colors.red,
+                        child: Text('Red'),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _controller.clear(); // Clear the content
+                      });
+                    },
+                    child: Text('Clear'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final signatureBytes = await _controller.toPngBytes();
+                      final signaturePath =
+                      await saveSignatureAsImage(signatureBytes!);
+                      // Call the new method for saving in the gallery
+                      await saveToGallery(signaturePath);
+                    },
+                    child: Text('Save to Gallery'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final signatureBytes = await _controller.toPngBytes();
+                      final signaturePath =
+                      await saveSignatureAsImage(signatureBytes!);
+                      await shareSignature(signaturePath);
+                    },
+                    child: Text('Share'),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
@@ -147,17 +158,83 @@ class _SignaturePadExampleState extends State<SignaturePadExample> {
     final File file = File(signaturePath);
     await file.writeAsBytes(signatureBytes);
 
-    await ImageGallerySaver.saveFile(signaturePath);
-
     return signaturePath;
   }
 
-  // Define the new method for sharing using the share_plus package
-  Future<void> shareSignatureImage(String imagePath) async {
+  Future<void> saveToGallery(String imagePath) async {
     try {
-      await Share.shareFiles([imagePath], text: 'Check out my signature!');
+      await ImageGallerySaver.saveFile(imagePath);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Signature saved to gallery')),
+      );
     } catch (e) {
-      print('Error sharing signature: $e');
+      print('Error saving signature: $e');
+    }
+  }
+
+  Future<void> shareSignature(String signaturePath) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Share Signature'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    await shareAsImage(signaturePath);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Share as Image'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await shareAsPdf(signaturePath);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Share as PDF'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> shareAsImage(String signaturePath) async {
+    try {
+      await Share.shareFiles([signaturePath], text: 'Check out my signature!');
+    } catch (e) {
+      print('Error sharing signature as image: $e');
+    }
+  }
+
+  Future<void> shareAsPdf(String signaturePath) async {
+    final pdf = pw.Document();
+    final image = pw.MemoryImage(File(signaturePath).readAsBytesSync());
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(image),
+          );
+        },
+      ),
+    );
+
+    final Uint8List pdfBytes = await pdf.save();
+    final tempDir = await getTemporaryDirectory();
+    final pdfPath = path.join(tempDir.path, 'signature.pdf');
+    final pdfFile = File(pdfPath);
+    await pdfFile.writeAsBytes(pdfBytes);
+
+    try {
+      await Share.shareFiles([pdfPath], text: 'Check out my signature PDF!');
+    } catch (e) {
+      print('Error sharing signature as PDF: $e');
     }
   }
 }
